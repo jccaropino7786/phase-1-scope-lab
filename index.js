69a408008c71f6d53c7520c5e6001f88260bf79b
john Caropino <jccaropino7786@live.com>
2022-11-25 19:11:55 -0800

    commit tree f467792827fa2e035bbf790467b64828a2230c0a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+// function returnCustomername(){
+//     return customerName
+// }
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}    
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = "JC"
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Matt'
+}
